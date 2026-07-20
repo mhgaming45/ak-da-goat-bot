6af943cbae303ff54bb7f6c1a84570af42d699be
mhgaming45 <marotimdpbn@gmail.com>
2026-07-20 19:16:57 +0530

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
