51210663f51a6bafa97e50c0e69e8a10b79c904a
mhgaming45 <marotimdpbn@gmail.com>
2026-07-20 19:17:44 +0530

    deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,27 @@
+require("dotenv").config();
 
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("panel")
+    .setDescription("Send the register panel")
+    .toJSON(),
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("Slash commands registered!");
+  } catch (err) {
+    console.error(err);
+  }
+})();
