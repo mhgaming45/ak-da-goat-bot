888fa3eadd9282d516915b14dc4d45a36a31abfb
mhgaming45 <marotimdpbn@gmail.com>
2026-07-21 22:10:22 +0530

    Refactor command handling in Discord bot

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const {
+  Client,
+  Collection,
+  GatewayIntentBits
+} = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+}
 
 client.once("ready", () => {
   console.log(`${client.user.tag} is online!`);
+});
+
+client.on("interactionCreate", async interaction => {
+  if (interaction.isChatInputCommand()) {
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 });
 
 client.login(process.env.TOKEN);
