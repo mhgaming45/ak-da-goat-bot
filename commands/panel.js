ab796695d756b5ba30618aeaa11f900c9052e3c9
mhgaming45 <marotimdpbn@gmail.com>
2026-07-23 10:36:56 +0530

    Update panel.js

--- commands/panel.js
+++ commands/panel.js
@@ -1,42 +1,14 @@
-const {
-  SlashCommandBuilder,
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle
-} = require("discord.js");
+const row2 = new ActionRowBuilder().addComponents(
+  new ButtonBuilder().setCustomId("uhc").setLabel("UHC").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("pot").setLabel("Pot").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("mace").setLabel("Mace").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("nethop").setLabel("NetHop").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("smp").setLabel("SMP").setStyle(ButtonStyle.Secondary)
+);
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("panel")
-    .setDescription("Send register panel"),
-
-  async execute(interaction) {
-
-    const embed = new EmbedBuilder()
-      .setTitle("📝 Register Testing")
-      .setDescription(
-        "Click **Register / Update** to register.\n\nChoose your gamemode below."
-      )
-      .setColor("Blue");
-
-    const row1 = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId("register")
-        .setLabel("Register / Update")
-        .setStyle(ButtonStyle.Danger)
-    );
-
-    const row2 = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId("uhc").setLabel("UHC").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("pot").setLabel("Pot").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("sword").setLabel("Sword").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("axe").setLabel("Axe").setStyle(ButtonStyle.Secondary)
-    );
-
-    await interaction.reply({
-      embeds: [embed],
-      components: [row1, row2]
-    });
-  }
-};
+const row3 = new ActionRowBuilder().addComponents(
+  new ButtonBuilder().setCustomId("sword").setLabel("Sword").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("axe").setLabel("Axe").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("vanilla").setLabel("Vanilla").setStyle(ButtonStyle.Secondary),
+  new ButtonBuilder().setCustomId("cart").setLabel("Cart").setStyle(ButtonStyle.Secondary)
+);
