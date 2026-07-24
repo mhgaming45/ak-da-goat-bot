3ae24ddfefacb791e740b0d6a01712e702c441c3
mhgaming45 <marotimdpbn@gmail.com>
2026-07-24 17:16:57 +0530

    Update panel.js

--- commands/panel.js
+++ commands/panel.js
@@ -14,37 +14,36 @@ module.exports = {
   async execute(interaction) {
 
     const embed = new EmbedBuilder()
-      .setTitle("📝 Register Testing")
+      .setTitle("📝 Player Registration")
       .setDescription(
-        "Click **Register / Update** to register.\n\nChoose your gamemode below."
+`Click the **Register / Update** button below.
+
+After completing the form, you'll be able to choose your gamemode.
+
+Available Gamemodes:
+• UHC
+• Pot
+• Mace
+• NetHop
+• SMP
+• Sword
+• Axe
+• Vanilla
+• Cart`
       )
       .setColor("Blue");
 
-    const row1 = new ActionRowBuilder().addComponents(
+    const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
         .setCustomId("register")
         .setLabel("Register / Update")
-        .setStyle(ButtonStyle.Danger)
-    );
-
-    const row2 = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId("uhc").setLabel("UHC").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("pot").setLabel("Pot").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("mace").setLabel("Mace").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("nethop").setLabel("NetHop").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("smp").setLabel("SMP").setStyle(ButtonStyle.Secondary)
-    );
-
-    const row3 = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId("sword").setLabel("Sword").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("axe").setLabel("Axe").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("vanilla").setLabel("Vanilla").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("cart").setLabel("Cart").setStyle(ButtonStyle.Secondary)
+        .setStyle(ButtonStyle.Success)
+        .setEmoji("📝")
     );
 
     await interaction.reply({
       embeds: [embed],
-      components: [row1, row2, row3]
+      components: [row]
     });
   }
 };
